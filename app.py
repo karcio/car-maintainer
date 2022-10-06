7160f979f46c67f06d9cef8bc4644208bae076f4
karcio <kczarnecki@hotmail.com>
2022-10-07 00:00:40 +0100

    [patch] flask template, bootstrap

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_login import LoginManager
 
 app = Flask(__name__)
 login_manager = LoginManager()
 
+
 @app.route("/")
-def hello_world():
-    return "<p>car maintainer app - in progress ...</p>"
+def main_app():
+    return render_template('index.html')

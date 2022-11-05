6ff731675808a24ae285eceb9e194e029e0181b2
karcio <kczarnecki@hotmail.com>
2022-11-05 00:39:14 +0000

    [patch] test commit from vim

--- app.py
+++ app.py
@@ -248,5 +248,5 @@ def cars_list():
 if __name__ == '__main__':
     app.secret_key = os.urandom(12)
     app.run(debug=True, host='0.0.0.0', port=5000)
-    # SSL
+    # SSL for Let's Encrypt
     #app.run(debug=False, host='0.0.0.0', port=5000, ssl_context=('/etc/letsencrypt/live/your-domain/fullchain.pem', '/etc/letsencrypt/live/your-domain/privkey.pem'))
